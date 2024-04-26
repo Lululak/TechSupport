fd9af34d5117e4515043aaf4ee7c4f39b131d570
Lululak <168170391+Lululak@users.noreply.github.com>
2024-04-26 15:02:07 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleFAQ(id) {
-    var element = document.getElementById(id);
-    if (element.style.display === 'block') {
-        element.style.display = 'none';
-    } else {
-        element.style.display = 'block';
-    }
-}
